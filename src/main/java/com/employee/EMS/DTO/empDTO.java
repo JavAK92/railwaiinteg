8269c6fb9dc442a0d2ecc35f3de227faeaa7e0e6
JavAK92 <88434576+JavAK92@users.noreply.github.com>
2024-04-28 16:43:04 +0300

    Update empDTO.java

--- src/main/java/com/employee/EMS/DTO/empDTO.java
+++ src/main/java/com/employee/EMS/DTO/empDTO.java
@@ -6,12 +6,9 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Data
 public class empDTO {
 	
 	
